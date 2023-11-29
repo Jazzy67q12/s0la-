67287739452e889648ab334d89644cfdad289b7d
Jazzy67q12 <148379283+Jazzy67q12@users.noreply.github.com>
2023-11-29 08:46:41 -0600

    Add files via upload

---
+++ bienvenido.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page Modern - MagtimusPro</title>
+
+    <link rel="stylesheet" href="http://localhost/inicio%20y%20registro/estilos.css">
+</head>
+<body>
+
+    <!-- Menu-->
+
+    <header>
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="img/n.png" alt="">
+                </a>
+            </div>
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contactos</a></li>
+                    </ul>
+                </nav>
+
+                <a href="http://localhost/inicio%20y%20registro/index.php" class="btn__quote">Iniciar sesión</a>
+
+                <div class="socialMedia">
+                    <a href="https://www.facebook.com/paul.macias.338">
+                        <img src="imagenes/facebook.png" alt="">
+                    </a>
+                    <a href="https://www.instagram.com/">
+                        <img src="imagenes/ig.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="assets/images/social media/twitter.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="assets/images/social media/youtube.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <!--INICIO-->
+        <div class="container__cover div__offset">
+            <div class="cover">
+                <section class="text__cover">
+                    <h1>Bienvenido a Solar Energy</h1>
+                    <p>A continuación se presentarán datos relevantes sobre la aplicación, junto con información crucial y valiosa acerca de la radiación.</p>
+                    <!--<a href="#" class="btn__text-cover btn__text">Empezar</a>-->
+                </section>
+                <section class="image__cover">
+                    <img src="imagenes/logo_blanco.png" alt="">
+                </section>
+            </div>
+        </div>
+
+        
+        <div class="container__trust container__card-primary">
+            <div class="trust card__primary">
+                <div class="text__trust text__card-primary">
+                    <p></p>
+                    <h1>Acerca de</h1>
+                </div>
+                <div class="container__trust container__box-cardPrimary">
+                    <div class="card__trust box__card-primary">
+                        <img src="" alt="">
+                        <h2>¿Que es?</h2>
+                        <p>Solar Energy es una aplicación web que sirve para medir la radiacion solar con herramientas de mediacion solar.</p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="assets/images/Trust area/archive.png" alt="">
+                        <h2>Objetivo</h2>
+                        <p>Disminuir los efectos de la radiación solar mediante un sistema de medición</p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="assets/images/Trust area/user.png" alt="">
+                        <h2>.</h2>
+                        <p>Poder ayudar al usuario y dar consejos sobre la proteccion contra la radiacion solar.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Sobre nosotros-->
+
+        <div class="container__about div__offset">
+            <div class="about">
+                <div class="text__about">
+                    <h1>Nosotros</h1>
+                    <p>...</p>
+                    <!--<a href="#" class="btn__text-about btn__text"></a>-->
+                </div>
+                
+                <div class="image__about">
+                    <img src="assets/images/About/about-1.png" alt="">
+                    <img src="assets/images/About/about-2.png" alt="">
+                </div>
+            </div>
+            
+        </div>
+
+    </main>
+    
+</body>
+</html>
